e963515a80ffe588a4c4f6d7efe8b3d62c006228
chenshuo <c1094829085@qq.com>
2025-09-03 23:06:39 +0200

    add 3 new datasets

--- src/breakoss/datasets/datasets.py
+++ src/breakoss/datasets/datasets.py
@@ -88,7 +88,7 @@ class StrongReject(BaseDataset):
 
 class HarmfulBehaviors(BaseDataset):
     def __init__(self, data_path: FilePath | str = None):
-        """Initialize the HarmfulBehaviors dataset from the GCG paper by loading data from CSV.
+        """Initialize the HarmfulBehaviors/AdvBench dataset from the GCG paper by loading data from CSV.
 
             @misc{zou2023universal,
               title={Universal and Transferable Adversarial Attacks on Aligned Language Models},
@@ -122,3 +122,107 @@ class HarmfulBehaviors(BaseDataset):
 
     def __len__(self) -> int:
         return 520
+
+class HarmBench(BaseDataset):
+    def __init__(self, data_path: FilePath | str = None):
+        """
+        Initialize the HarmBench dataset from the HarmBench paper by loading data from CSV.
+        https://github.com/centerforaisafety/HarmBench/blob/main/data/behavior_datasets/harmbench_behaviors_text_all.csv
+        @article{mazeika2024harmbench,
+          title={HarmBench: A Standardized Evaluation Framework for Automated Red Teaming and Robust Refusal},
+          author={Mantas Mazeika and Long Phan and Xuwang Yin and Andy Zou and Zifan Wang and Norman Mu and Elham Sakhaee and Nathaniel Li and Steven Basart and Bo Li and David Forsyth and Dan Hendrycks},
+          year={2024},
+          eprint={2402.04249},
+          archivePrefix={arXiv},
+          primaryClass={cs.LG}
+        }
+        """
+
+        if data_path is None:
+            data_path = os.path.join(os.path.dirname(__file__), "harmbench_behaviors_text_all.csv")
+        self.pd = pd.read_csv(data_path)
+
+    def __iter__(self) -> Iterable[DataSample]:
+        """Lazily iterate over data samples from the HarmBench dataset.
+
+        Yields:
+            DataSample: Data samples with harmful prompts and metadata.
+        """
+        for index, row in self.pd.iterrows():
+            yield DataSample(
+                harmful_prompt=row["Behavior"],
+                target="",
+                metadata={"category": row["SemanticCategory"], "source": "harmbench"},
+            )
+
+    def name(self) -> str:
+        return "harmbench"
+
+    def __len__(self) -> int:
+        return 400
+
+class CatQA(BaseDataset):
+    """Dataset for CatQA prompts."""
+
+    def __init__(self, data_path: FilePath | str = None):
+        """Initialize the CatQA dataset by loading data from CSV."""
+        if data_path is None:
+            data_path = os.path.join(os.path.dirname(__file__), "catqa_english.json")
+        self.data_items = []
+        with open(data_path, "r") as f:
+            for line in f.readlines():
+                self.data_items.append(json.loads(line))
+
+
+    def name(self) -> str:
+        return "catqa"
+
+    def __len__(self) -> int:
+        return 550
+
+    def __iter__(self) -> Iterable[DataSample]:
+        """Lazily iterate over data samples from the CatQA dataset.
+
+        Yields:
+            DataSample: Data samples with harmful prompts and metadata.
+        """
+        for item in self.data_items:
+            yield DataSample(
+                harmful_prompt=item["Question"],
+                target="",
+                metadata={"category": item.get("Category", ""),
+                          "sub-category": item.get("Subcategory", ""),
+                          "source": "catqa"
+                          },
+            )
+
+class JBBHarmfulBehaviors(BaseDataset):
+    """Dataset for JBB Behaviors prompts."""
+
+    def __init__(self, data_path: FilePath | str = None):
+        """Initialize the JBB Behaviors dataset by loading data from CSV."""
+        if data_path is None:
+            data_path = os.path.join(os.path.dirname(__file__), "jbb-harmful-behaviors.csv")
+        self.pd = pd.read_csv(data_path)
+
+    def name(self) -> str:
+        return "jbb_harmful_behaviors"
+
+    def __len__(self) -> int:
+        return 100
+
+    def __iter__(self) -> Iterable[DataSample]:
+        """Lazily iterate over data samples from the JBB Behaviors dataset.
+
+        Yields:
+            DataSample: Data samples with harmful prompts and metadata.
+        """
+        for index, row in self.pd.iterrows():
+            yield DataSample(
+                harmful_prompt=row["Goal"],
+                target=row["Target"],
+                metadata={"category": row.get("Category", ""),
+                          "behavior": row.get("Behavior", ""),
+                          "source": "jbb_behaviors"
+                          },
+            )
