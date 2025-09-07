da71aecb43c4095d000af0739051eb179465c248
chenshuo <c1094829085@qq.com>
2025-09-07 11:51:22 +0200

    support 5 datasets

--- src/breakoss/datasets/__init__.py
+++ src/breakoss/datasets/__init__.py
@@ -1,3 +1,11 @@
-from .datasets import StrongReject, HarmfulBehaviors, DataSample, BaseDataset
+from .datasets import (
+    StrongReject,
+    HarmfulBehaviors,
+    HarmBench,
+    JBBHarmfulBehaviors,
+    CatQA,
+    DataSample,
+    BaseDataset
+)
 
-__all__ = ["StrongReject", "HarmfulBehaviors", "DataSample", "BaseDataset"]
+__all__ = ["StrongReject", "HarmfulBehaviors", "DataSample", "BaseDataset", "HarmBench", "JBBHarmfulBehaviors", "CatQA"]
--- src/breakoss/datasets/datasets.py
+++ src/breakoss/datasets/datasets.py
@@ -2,11 +2,15 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Iterable
+from typing import Iterable, Callable
 from pydantic import BaseModel, FilePath
 import pandas as pd
+from ..methods import BaseMethod
+import pathlib
+import json
 import os
-
+from loguru import logger
+from tqdm import tqdm
 
 class DataSample(BaseModel):
     """A single data sample containing a harmful prompt and associated metadata."""
@@ -40,6 +44,31 @@ class BaseDataset(ABC):
     def __len__(self) -> int:
         raise NotImplementedError()
 
+    @abstractmethod
+    def __getitem__(self, index: int) -> DataSample:
+        """Get a specific data sample by index.
+
+        Args:
+            index (int): Index of the data sample.
+
+        Returns:
+            DataSample: The data sample at the specified index.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def apply_transformation(self, jailbreak_method: BaseMethod, stored_path: pathlib.Path, batch_size: int = 1):
+        """Apply a transformation function to each data sample in the dataset.
+
+        Args:
+            jailbreak_method (BaseMethod): Jailbreak Method Class to transform each data sample.
+            stored_path (pathlib.Path): Path to store the transformed data sample.
+            batch_size (int, optional): Batch size for transformation. Defaults to 1.
+        Returns:
+            BaseDataset: A new dataset with transformed data samples.
+        """
+        raise NotImplementedError()
+
 
 class StrongReject(BaseDataset):
     """Dataset for strong reject prompts loaded from CSV.
@@ -58,10 +87,10 @@ class StrongReject(BaseDataset):
     def __init__(self, data_path: FilePath | str = None):
         """Initialize the StrongReject dataset by loading data from CSV."""
         if data_path is None:
-            data_path = os.path.join(
+            self.data_path = os.path.join(
                 os.path.dirname(__file__), "strongreject_dataset.csv"
             )
-        self.pd = pd.read_csv(data_path)
+        self.pd = pd.read_csv(self.data_path)
 
     def name(self) -> str:
         return "strong_reject"
@@ -84,7 +113,42 @@ class StrongReject(BaseDataset):
                     "source": "strong_reject_" + row["source"],
                 },
             )
+    def __getitem__(self, index: int) -> DataSample:
+        """Get a specific data sample by index.
 
+        Args:
+            index (int): Index of the data sample.
+
+        Returns:
+            DataSample: The data sample at the specified index.
+        """
+        row = self.pd.iloc[index]
+        return DataSample(
+            harmful_prompt=row["forbidden_prompt"],
+            target="",
+            metadata={
+                "category": row["category"],
+                "source": "strong_reject_" + row["source"],
+            },
+        )
+
+    def apply_transformation(self, jailbreak_method: BaseMethod, stored_path: pathlib.Path, batch_size: int = 1):
+        self.pd["forbidden_prompt_original"] = self.pd["forbidden_prompt"]
+        new_prompts = [""]*len(self.pd["forbidden_prompt"])
+        original_data_file_name, suffix = self.data_path.split("/")[-1].split(".")
+        original_data_file_name = (
+                original_data_file_name + "_" + jailbreak_method.name() + "." + suffix
+        )
+        for i in tqdm(range(0, len(self.pd), batch_size)):
+            batch = self.pd["forbidden_prompt_original"].iloc[i:i + batch_size].tolist()
+            transformed = jailbreak_method.jailbreak_transform_batch(batch)
+            for j, idx in enumerate(range(i, min(i + batch_size, len(self.pd)))):
+                new_prompts[idx] = transformed[j]
+            self.pd["forbidden_prompt"] = pd.concat([pd.Series(new_prompts)], ignore_index=True)
+            self.pd.to_csv(
+                os.path.join(stored_path, original_data_file_name), index=False
+            )
+        logger.info(f"Transformed data saved to {os.path.join(stored_path, original_data_file_name)}")
 
 class HarmfulBehaviors(BaseDataset):
     def __init__(self, data_path: FilePath | str = None):
@@ -101,8 +165,8 @@ class HarmfulBehaviors(BaseDataset):
 
         """
         if data_path is None:
-            data_path = os.path.join(os.path.dirname(__file__), "harmful_behaviors.csv")
-        self.pd = pd.read_csv(data_path)
+            self.data_path = os.path.join(os.path.dirname(__file__), "harmful_behaviors.csv")
+        self.pd = pd.read_csv(self.data_path)
 
     def __iter__(self) -> Iterable[DataSample]:
         """Lazily iterate over data samples from the harmful behaviors dataset.
@@ -123,6 +187,41 @@ class HarmfulBehaviors(BaseDataset):
     def __len__(self) -> int:
         return 520
 
+    def __getitem__(self, index: int) -> DataSample:
+        """Get a specific data sample by index.
+
+        Args:
+            index (int): Index of the data sample.
+
+        Returns:
+            DataSample: The data sample at the specified index.
+        """
+        row = self.pd.iloc[index]
+        return DataSample(
+            harmful_prompt=row["goal"],
+            target=row["target"],
+            metadata={"source": "harmful_behavior_gcg"},
+        )
+
+    def apply_transformation(self, jailbreak_method: BaseMethod, stored_path: pathlib.Path, batch_size: int = 16):
+        self.pd["goal_original"] = self.pd["goal"]
+        new_prompts = [""]*len(self.pd["goal"])
+        original_data_file_name, suffix = self.data_path.split("/")[-1].split(".")
+        original_data_file_name = (
+                original_data_file_name + "_" + jailbreak_method.name() + "." + suffix
+        )
+
+        for i in tqdm(range(0, len(self.pd), batch_size)):
+            batch = self.pd["goal_original"].iloc[i:i + batch_size].tolist()
+            transformed = jailbreak_method.jailbreak_transform_batch(batch)
+            for j, idx in enumerate(range(i, min(i + batch_size, len(self.pd)))):
+                new_prompts[idx] = transformed[j]
+            self.pd["goal"] = pd.concat([pd.Series(new_prompts)], ignore_index=True)
+            self.pd.to_csv(
+                os.path.join(stored_path, original_data_file_name), index=False
+            )
+        logger.info(f"Transformed data saved to {os.path.join(stored_path, original_data_file_name)}")
+
 class HarmBench(BaseDataset):
     def __init__(self, data_path: FilePath | str = None):
         """
@@ -139,8 +238,8 @@ class HarmBench(BaseDataset):
         """
 
         if data_path is None:
-            data_path = os.path.join(os.path.dirname(__file__), "harmbench_behaviors_text_all.csv")
-        self.pd = pd.read_csv(data_path)
+            self.data_path = os.path.join(os.path.dirname(__file__), "harmbench_behaviors_text_all.csv")
+        self.pd = pd.read_csv(self.data_path)
 
     def __iter__(self) -> Iterable[DataSample]:
         """Lazily iterate over data samples from the HarmBench dataset.
@@ -155,11 +254,47 @@ class HarmBench(BaseDataset):
                 metadata={"category": row["SemanticCategory"], "source": "harmbench"},
             )
 
+    def __getitem__(self, index: int) -> DataSample:
+        """Get a specific data sample by index.
+
+        Args:
+            index (int): Index of the data sample.
+
+        Returns:
+            DataSample: The data sample at the specified index.
+        """
+        row = self.pd.iloc[index]
+        return DataSample(
+            harmful_prompt=row["Behavior"],
+            target="",
+            metadata={"category": row["SemanticCategory"], "source": "harmbench"},
+        )
+
+    def apply_transformation(self, jailbreak_method: BaseMethod, stored_path: pathlib.Path, batch_size: int = 16):
+        self.pd["Behavior_original"] = self.pd["Behavior"]
+        new_prompts = [""]*len(self.pd["Behavior"])
+        original_data_file_name, suffix = self.data_path.split("/")[-1].split(".")
+        original_data_file_name = (
+                original_data_file_name + "_" + jailbreak_method.name() + "." + suffix
+        )
+
+        for i in tqdm(range(0, len(self.pd), batch_size)):
+            batch = self.pd["Behavior_original"].iloc[i:i + batch_size].tolist()
+            transformed = jailbreak_method.jailbreak_transform_batch(batch)
+            for j, idx in enumerate(range(i, min(i + batch_size, len(self.pd)))):
+                new_prompts[idx] = transformed[j]
+            self.pd["Behavior"] = pd.concat([pd.Series(new_prompts)], ignore_index=True)
+            self.pd.to_csv(
+                os.path.join(stored_path, original_data_file_name), index=False
+            )
+        logger.info(f"Transformed data saved to {os.path.join(stored_path, original_data_file_name)}")
+
     def name(self) -> str:
         return "harmbench"
 
     def __len__(self) -> int:
         return 400
+
 
 class CatQA(BaseDataset):
     """Dataset for CatQA prompts."""
@@ -167,9 +302,9 @@ class CatQA(BaseDataset):
     def __init__(self, data_path: FilePath | str = None):
         """Initialize the CatQA dataset by loading data from CSV."""
         if data_path is None:
-            data_path = os.path.join(os.path.dirname(__file__), "catqa_english.json")
+            self.data_path = os.path.join(os.path.dirname(__file__), "catqa_english.json")
         self.data_items = []
-        with open(data_path, "r") as f:
+        with open(self.data_path, "r") as f:
             for line in f.readlines():
                 self.data_items.append(json.loads(line))
 
@@ -196,14 +331,54 @@ class CatQA(BaseDataset):
                           },
             )
 
+    def __getitem__(self, index: int) -> DataSample:
+        """Get a specific data sample by index.
+
+        Args:
+            index (int): Index of the data sample.
+
+        Returns:
+            DataSample: The data sample at the specified index.
+        """
+        item = self.data_items[index]
+        return DataSample(
+            harmful_prompt=item["Question"],
+            target="",
+            metadata={"category": item.get("Category", ""),
+                      "sub-category": item.get("Subcategory", ""),
+                      "source": "catqa"
+                      },
+        )
+
+    def apply_transformation(self, jailbreak_method: BaseMethod, stored_path: pathlib.Path, batch_size: int = 1):
+        original_data_file_name, suffix = self.data_path.split("/")[-1].split(".")
+        original_data_file_name = (
+                original_data_file_name + "_" + jailbreak_method.name() + "." + suffix
+        )
+
+        for item in self.data_items:
+            item["Question_original"] = item["Question"]
+            item["Question"] = ""
+
+        for i in tqdm(range(0, len(self.data_items), batch_size)):
+            batch = [self.data_items[j]["Question_original"] for j in range(i, min(i + batch_size, len(self.data_items)))]
+            transformed = jailbreak_method.jailbreak_transform_batch(batch)
+            for j, idx in enumerate(range(i, min(i + batch_size, len(self.data_items)))):
+                self.data_items[idx]["Question"] = transformed[j]
+
+            with open(os.path.join(stored_path, original_data_file_name), "w") as f:
+                for item in self.data_items:
+                    f.write(json.dumps(item) + "\n")
+        logger.info(f"Transformed data saved to {os.path.join(stored_path, original_data_file_name)}")
+
 class JBBHarmfulBehaviors(BaseDataset):
     """Dataset for JBB Behaviors prompts."""
 
     def __init__(self, data_path: FilePath | str = None):
         """Initialize the JBB Behaviors dataset by loading data from CSV."""
         if data_path is None:
-            data_path = os.path.join(os.path.dirname(__file__), "jbb-harmful-behaviors.csv")
-        self.pd = pd.read_csv(data_path)
+            self.data_path = os.path.join(os.path.dirname(__file__), "jbb-harmful-behaviors.csv")
+        self.pd = pd.read_csv(self.data_path)
 
     def name(self) -> str:
         return "jbb_harmful_behaviors"
@@ -226,3 +401,41 @@ class JBBHarmfulBehaviors(BaseDataset):
                           "source": "jbb_behaviors"
                           },
             )
+
+    def __getitem__(self, index: int) -> DataSample:
+        """Get a specific data sample by index.
+
+        Args:
+            index (int): Index of the data sample.
+
+        Returns:
+            DataSample: The data sample at the specified index.
+        """
+        row = self.pd.iloc[index]
+        return DataSample(
+            harmful_prompt=row["Goal"],
+            target=row["Target"],
+            metadata={"category": row.get("Category", ""),
+                      "behavior": row.get("Behavior", ""),
+                      "source": "jbb_behaviors"
+                      },
+        )
+
+    def apply_transformation(self, jailbreak_method: BaseMethod, stored_path: pathlib.Path, batch_size: int = 16):
+        self.pd["Goal_original"] = self.pd["Goal"]
+        new_prompts = [""]*len(self.pd["Goal"])
+
+        original_data_file_name, suffix = self.data_path.split("/")[-1].split(".")
+        original_data_file_name = (
+            original_data_file_name + "_" + jailbreak_method.name() + "." + suffix
+        )
+        for i in tqdm(range(0, len(self.pd), batch_size)):
+            batch = self.pd["Goal_original"].iloc[i:i + batch_size].tolist()
+            transformed = jailbreak_method.jailbreak_transform_batch(batch)
+            for j, idx in enumerate(range(i, min(i + batch_size, len(self.pd)))):
+                new_prompts[idx] = transformed[j]
+            self.pd["Goal"] = pd.concat([pd.Series(new_prompts)], ignore_index=True)
+            self.pd.to_csv(
+                os.path.join(stored_path, original_data_file_name), index=False
+            )
+        logger.info(f"Transformed data saved to {os.path.join(stored_path, original_data_file_name)}")
