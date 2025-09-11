81ae8062e63a316afb5c08cd9c612c439f165bd9
chenshuo <c1094829085@qq.com>
2025-09-11 11:38:24 +0200

    add main inference and eval

--- examples/4_intent_hijack.py
+++ examples/4_intent_hijack.py
@@ -1,13 +1,18 @@
 
+from breakoss.methods import FakeOverrefusal, CoTBypass, IntentHijack
 from typing import Literal
-from breakoss import go_jailbreak, go_evaluate
+from breakoss import go_jailbreak, go_evaluate_on_all_metrics
 import fire
 from breakoss.models import load_llm, InferenceConfig
-from breakoss.methods import IntentHijack
-from breakoss.evaluators import LlamaGuardEvaluator, RefusalWordsEvaluator
-from breakoss.datasets import StrongReject, HarmfulBehaviors
+from breakoss.methods import CoTBypass
+from breakoss.evaluators import RefusalWordsEvaluator, LlamaGuardEvaluator, StrongREJECTEvaluator, HarmBenchEvaluator, HarmBenchEvaluator
+from breakoss.datasets import StrongReject, HarmfulBehaviors, HarmBench, CatQA, JBBHarmfulBehaviors
+from breakoss.const import  SUPPORTED_MODELS, SUPPORTED_DATASETS, SUPPORTED_METRICS
+from breakoss.utils import free_gpu_memory
+
 from loguru import logger
 import torch
+import os
 import gc
 import json
 
@@ -71,6 +76,81 @@ def eval_on_strongreject(
         seed=seed,
         inference_config=inference_config,
     )
+
+
+
+def main_inference(
+    dataset_name: Literal["HarmfulBehaviors", "StrongReject", "HarmBench", "CatQA", "JBBHarmfulBehaviors"] = "HarmfulBehaviors",
+    model_name: Literal[SUPPORTED_MODELS] = "openai/gpt-oss-20b",
+    starting_index: int = 0,
+    end_index: int = -1,
+    batch_size: int = 4,
+    seed: int = 42,
+):
+    """Only inference to collect the output, eval will be done separately"""
+    assert model_name in SUPPORTED_MODELS, f"Model {model_name} is not supported. Supported models are: {SUPPORTED_MODELS}"
+    model = load_llm(model_name=model_name, cuda_device="auto", provider="hf")
+    judge = None
+    inference_config = InferenceConfig(
+        max_new_tokens=1000,
+        temperature=0.0,
+        top_p=0.9,
+        repetition_penalty=1.3,
+        do_sample=False,
+    )
+
+    logger.info(f"Loading transformed dataset {dataset_name} for IntentHijack method.")
+    base_dir = os.path.join(os.path.dirname(__file__), "../transformed_datasets")
+    if dataset_name == "StrongReject":
+        dataset = StrongReject(data_path=os.path.join(base_dir, "strongreject_dataset_IntentHijack.csv"))
+    elif dataset_name == "HarmfulBehaviors":
+        dataset = HarmfulBehaviors(data_path=os.path.join(base_dir, "harmful_behaviors_IntentHijack.csv"))
+    elif dataset_name == "HarmBench":
+        dataset = HarmBench(data_path=os.path.join(base_dir, "harmbench_behaviors_text_all_IntentHijack.csv"))
+    elif dataset_name == "CatQA":
+        dataset = CatQA(data_path=os.path.join(base_dir, "catqa_english_IntentHijack.json"))
+    elif dataset_name == "JBBHarmfulBehaviors":
+        dataset = JBBHarmfulBehaviors(data_path=os.path.join(base_dir, "jbb-harmful-behaviors_IntentHijack.csv"))
+    else:
+        raise ValueError(f"Unknown dataset name: {dataset_name}")
+
+    exp = go_jailbreak(
+        model=model, method=None, evaluator=judge, harmful_prompts=dataset, inference_config=inference_config,
+        batch_size=batch_size,
+        starting_index=starting_index, end_index=end_index, seed=seed
+    )
+
+    free_gpu_memory(model)
+    record_dir = exp.log_dir
+    logger.info(f"Records are saved in {record_dir}")
+    go_evaluate_on_all_metrics(record_dir)
+
+
+def transform_original_prompts(
+    dataset_name: str,
+):
+
+    if dataset_name == "StrongReject":
+        dataset = StrongReject()
+    elif dataset_name == "HarmfulBehaviors":
+        dataset = HarmfulBehaviors()
+    elif dataset_name == "HarmBench":
+        dataset = HarmBench()
+    elif dataset_name == "CatQA":
+        dataset = CatQA()
+    elif dataset_name == "JBBHarmfulBehaviors":
+        dataset = JBBHarmfulBehaviors()
+    else:
+        raise ValueError(f"Unknown dataset name: {dataset_name}")
+
+    intent_hijack = IntentHijack()
+    logger.info(f"Transforming {len(dataset)} prompts from {dataset_name} using FakeOverrefusal method.")
+    dataset.apply_transformation(
+        intent_hijack,
+        stored_path=os.path.join(os.path.dirname(__file__), "../transformed_datasets"),
+        batch_size=8,
+    )
+
 
 
 if __name__ == "__main__":
