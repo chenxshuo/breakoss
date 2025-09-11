1e901616c98cd927276f87386b65c8a41a0f35c4
chenshuo <c1094829085@qq.com>
2025-09-11 11:39:15 +0200

    check mismatch between different metrics

---
+++ examples/try_eval_mismatch.py
@@ -0,0 +1,111 @@
+"""Compare HarmBenchEvaluator vs. StrongRejectEvaluator"""
+import os
+import json
+import fire
+
+def harmbench_vs_strongreject():
+    base_dir = "breakoss_logs/openai_gpt-oss-20b/strong_reject_[0_-1]/CoTBypass/pure_inference_no_eval/seed_42/2025-09-10_17-48-57/re_evaluation_records_before_evaluation/"
+    harmbench_evaluator_record = os.path.join(base_dir, "experiment_summary_reevaluate_by_HarmBenchEvaluator.json")
+    strongreject_evaluator_record = os.path.join(base_dir, "experiment_summary_reevaluate_by_StrongREJECTEvaluator.json")
+
+
+    with open(harmbench_evaluator_record, 'r') as f:
+        harmbench_evaluator_record = json.load(f)["record_list"]
+
+    with open(strongreject_evaluator_record, 'r') as f:
+        strongreject_evaluator_record = json.load(f)["record_list"]
+
+    sr_harmful_hb_harmful = 0
+    rephrased_faile = 0
+    sr_harmful_hb_safe = 0
+    sr_safe_hb_harmful = 0
+    sr_safe_hb_safe = 0
+    sr_harmful_hb_safe_sr_bigger = 0
+    for harmbench_item, strongreject_item in zip(harmbench_evaluator_record, strongreject_evaluator_record):
+        assert harmbench_item["datasample"]["original_harmful_prompt"] == strongreject_item["datasample"]["original_harmful_prompt"]
+        harmbench_judge = harmbench_item["evaluate_result"]["is_harmful"]
+        strongreject_judge = strongreject_item["evaluate_result"]["is_harmful"]
+        if harmbench_judge == False and strongreject_judge == True:
+            sr_harmful_hb_safe += 1
+            if  strongreject_item["evaluate_result"]["harmful_score"] > 0.1:
+                sr_harmful_hb_safe_sr_bigger += 1
+        elif harmbench_judge == True and strongreject_judge == True:
+            sr_harmful_hb_harmful += 1
+        elif harmbench_judge == False and strongreject_judge == False:
+            sr_safe_hb_safe += 1
+            if "<think>" in strongreject_item["datasample"]["harmful_prompt"]:
+                rephrased_faile +=1
+            else:
+                print(strongreject_item["datasample"]["original_harmful_prompt"])
+                print(strongreject_item["datasample"]["harmful_prompt"])
+                print("Response:", strongreject_item["evaluate_result"]["response"])
+                print("___________________________")
+            # print("Safe by both:")
+
+
+        elif harmbench_judge == True and strongreject_judge == False:
+            sr_safe_hb_harmful += 1
+    print(f"StrongReject harmful & HarmBench harmful: {sr_harmful_hb_harmful}")
+    print(f"StrongReject harmful & HarmBench safe: {sr_harmful_hb_safe}")
+    print(f"StrongReject harmful & HarmBench safe but StrongReject score > 0.5: {sr_harmful_hb_safe_sr_bigger}")
+    print(f"StrongReject safe & HarmBench harmful: {sr_safe_hb_harmful}")
+    print(f"StrongReject safe & HarmBench safe: {sr_safe_hb_safe}")
+    print(f"Rephrased failed (safe by both but contains <think>): {rephrased_faile}")
+    print(f"Total samples: {len(harmbench_evaluator_record)}")
+
+
+def llamaguard_vs_strongreject():
+    base_dir = "breakoss_logs/openai_gpt-oss-20b/strong_reject_[0_-1]/CoTBypass/pure_inference_no_eval/seed_42/2025-09-10_17-48-57/re_evaluation_records_before_evaluation/"
+    llamaguard_evaluator_record = os.path.join(base_dir, "experiment_summary_reevaluate_by_LlamaGuardEvaluator.json")
+    strongreject_evaluator_record = os.path.join(base_dir, "experiment_summary_reevaluate_by_StrongREJECTEvaluator.json")
+
+
+    with open(llamaguard_evaluator_record, 'r') as f:
+        llamaguard_evaluator_record = json.load(f)["record_list"]
+
+    with open(strongreject_evaluator_record, 'r') as f:
+        strongreject_evaluator_record = json.load(f)["record_list"]
+
+    sr_harmful_hb_harmful = 0
+    rephrased_faile = 0
+    sr_harmful_hb_safe = 0
+    sr_safe_hb_harmful = 0
+    sr_safe_hb_safe = 0
+    sr_harmful_hb_safe_sr_bigger = 0
+    for llamaguard_item, strongreject_item in zip(llamaguard_evaluator_record, strongreject_evaluator_record):
+        assert llamaguard_item["datasample"]["original_harmful_prompt"] == strongreject_item["datasample"]["original_harmful_prompt"]
+        llamaguard_judge = llamaguard_item["evaluate_result"]["is_harmful"]
+        strongreject_judge = strongreject_item["evaluate_result"]["is_harmful"]
+        if llamaguard_judge == False and strongreject_judge == True:
+            sr_harmful_hb_safe += 1
+            if  strongreject_item["evaluate_result"]["harmful_score"] > 0.5:
+                sr_harmful_hb_safe_sr_bigger += 1
+                print(strongreject_item["datasample"]["original_harmful_prompt"])
+                print(strongreject_item["datasample"]["harmful_prompt"])
+                print("Response:", strongreject_item["evaluate_result"]["response"])
+                print("___________________________")
+        elif llamaguard_judge == True and strongreject_judge == True:
+            sr_harmful_hb_harmful += 1
+        elif llamaguard_judge == False and strongreject_judge == False:
+            sr_safe_hb_safe += 1
+            # if "<think>" in strongreject_item["datasample"]["harmful_prompt"]:
+            #     rephrased_faile += 1
+            # else:
+            #     print(strongreject_item["datasample"]["original_harmful_prompt"])
+            #     print(strongreject_item["datasample"]["harmful_prompt"])
+            #     print("Response:", strongreject_item["evaluate_result"]["response"])
+            #     print("___________________________")
+            # print("Safe by both:")
+
+        elif llamaguard_judge == True and strongreject_judge == False:
+            sr_safe_hb_harmful += 1
+    print(f"StrongReject harmful & LlamaGuard harmful: {sr_harmful_hb_harmful}")
+    print(f"StrongReject harmful & LlamaGuard safe: {sr_harmful_hb_safe}")
+    print(f"StrongReject harmful & LlamaGuard safe but StrongReject score > 0.5: {sr_harmful_hb_safe_sr_bigger}")
+    print(f"StrongReject safe & LlamaGuard harmful: {sr_safe_hb_harmful}")
+    print(f"StrongReject safe & LlamaGuard safe: {sr_safe_hb_safe}")
+    print(f"Rephrased failed (safe by both but contains <think>): {rephrased_faile}")
+
+
+if __name__ == "__main__":
+    fire.Fire()
