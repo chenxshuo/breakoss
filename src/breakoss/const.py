99b0cd891218f4d24510bce57cb60a704b2f9083
chenshuo <c1094829085@qq.com>
2025-09-11 11:33:15 +0200

    update const

--- src/breakoss/const.py
+++ src/breakoss/const.py
@@ -1,14 +1,14 @@
 SUPPORTED_MODELS = [
     "openai/gpt-oss-20b",
     "openai/gpt-oss-120b",
-    "microsoft/phi-4-reasoning",
-    "qwen/qwen3-4b-thinking-2507",
-    "RealSafe/RealSafe-R1-7B"
+    # "microsoft/phi-4-reasoning",
+    # "qwen/qwen3-4b-thinking-2507",
+    # "RealSafe/RealSafe-R1-7B"
 ]
 
 SUPPORTED_DATASETS = [
-    "HarmfulBehaviors",
     "StrongReject",
+    "HarmfulBehaviors",
     "HarmBench",
     "CatQA",
     "JBBHarmfulBehaviors"
