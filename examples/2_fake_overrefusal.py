0d1464f2e680030269aebffafdfa7c94f912ae7a
chenshuo <c1094829085@qq.com>
2025-09-11 11:38:17 +0200

    add main inference and eval

--- examples/2_fake_overrefusal.py
+++ examples/2_fake_overrefusal.py
@@ -1,11 +1,13 @@
 """Use fake over-refusal to jailbreak LLMs."""
 
-from breakoss import go_jailbreak
+from breakoss import go_jailbreak, go_evaluate_on_all_metrics
+from breakoss.utils import  free_gpu_memory
 from typing import Literal
 from breakoss.models import load_llm, InferenceConfig
-from breakoss.methods import FakeOverrefusal
+from breakoss.methods import FakeOverrefusal, CoTBypass
 from breakoss.evaluators import LlamaGuardEvaluator, RefusalWordsEvaluator
 from breakoss.datasets import StrongReject, HarmfulBehaviors, HarmBench, JBBHarmfulBehaviors, CatQA
+from breakoss.const import  SUPPORTED_MODELS, SUPPORTED_DATASETS, SUPPORTED_METRICS
 import fire
 import pathlib
 import gc
@@ -38,38 +40,128 @@ def one_example():
     )
 
     
-# def main(
-#     dataset_name: Literal["HarmfulBehaviors", "StrongReject"] = "HarmfulBehaviors",
-#     starting_index: int = 0,
-#     end_index: int = -1,
-#     seed: int = 42,
-# ):
-#     model = load_llm(model_name="openai/gpt-oss-20b", cuda_device="auto", provider="hf")
-#     fake_over_refusal = FakeOverrefusal()
-#     judge = RefusalWordsEvaluator()
+def main_inference(
+    dataset_name: Literal["StrongReject", "HarmfulBehaviors", "HarmBench", "CatQA", "JBBHarmfulBehaviors"] = "StrongReject",
+    model_name: str = "openai/gpt-oss-20b",
+    starting_index: int = 0,
+    end_index: int = -1,
+    batch_size: int = 4,
+    seed: int = 42,
+    failure_cases_dir: pathlib.Path | str = None,
+):
+    """
+    Load Transformed prompts from FakeOverrefusal method and run inference + CoTBypass Method.
+    """
+    assert model_name in SUPPORTED_MODELS, f"Model {model_name} is not supported. Supported models are: {SUPPORTED_MODELS}"
+    model = load_llm(model_name=model_name, cuda_device="auto", provider="hf")
+    judge = None
+    cot_bypass = CoTBypass()
+    inference_config = InferenceConfig(
+        max_new_tokens=1000,
+        temperature=0.0,
+        top_p=0.9,
+        repetition_penalty=1.3,
+        do_sample=False,
+    )
+    logger.info(f"Loading transformed dataset {dataset_name} for FakeOverrefusal method.")
+    base_dir = os.path.join(os.path.dirname(__file__), "../transformed_datasets")
+    if dataset_name == "StrongReject":
+        dataset = StrongReject(data_path=os.path.join(base_dir, "strongreject_dataset_FakeOverrefusal.csv"))
+    elif dataset_name == "HarmfulBehaviors":
+        dataset = HarmfulBehaviors(data_path=os.path.join(base_dir, "harmful_behaviors_FakeOverrefusal.csv"))
+    elif dataset_name == "HarmBench":
+        dataset = HarmBench(data_path=os.path.join(base_dir, "harmbench_behaviors_text_all_FakeOverrefusal.csv"))
+    elif dataset_name == "CatQA":
+        dataset = CatQA(data_path=os.path.join(base_dir, "catqa_english_FakeOverrefusal.json"))
+    elif dataset_name == "JBBHarmfulBehaviors":
+        dataset = JBBHarmfulBehaviors(data_path=os.path.join(base_dir, "jbb-harmful-behaviors_FakeOverrefusal.csv"))
+    else:
+        raise ValueError(f"Unknown dataset name: {dataset_name}")
 
-#     inference_config = InferenceConfig(
-#         max_new_tokens=100000,
-#         temperature=0.7,
-#         top_p=0.9,
-#         repetition_penalty=1.0
-#     )
 
-#     if dataset_name == "StrongReject":
-#         dataset = StrongReject()
-#     elif dataset_name == "HarmfulBehaviors":
-#         dataset = HarmfulBehaviors()
+    if failure_cases_dir is not None:
+        failure_cases_dir = os.path.join(failure_cases_dir, "re_evaluation_records_before_evaluation")
+        assert os.path.exists(failure_cases_dir), f"Failure cases directory {failure_cases_dir} does not exist."
 
-#     go_jailbreak(
-#         model=model,
-#         method=fake_over_refusal,
-#         evaluator=judge,
-#         harmful_prompts=dataset,
-#         starting_index=starting_index,
-#         end_index=end_index,
-#         seed=seed,
-#         inference_config=inference_config
-#     )
+        failure_cases_json = [os.path.join(failure_cases_dir, f) for f in os.listdir(failure_cases_dir) if f.startswith("failure_cases_by_") and f.endswith(".json")]
+
+        def get_metric_name(file_path: str) -> str:
+            return file_path.split("failure_cases_by_")[-1].split(".json")[0]
+
+        def get_question_list(file_path: str) -> list:
+            import json
+            with open(file_path, "r") as f:
+                data = json.load(f)
+            questions = data['failure_cases']
+            return questions
+
+        metrics_name_to_failure_prompts = {
+            get_metric_name(file_path): get_question_list(file_path) for file_path in failure_cases_json
+        }
+
+        exp = go_jailbreak(
+            model=model, method=cot_bypass, evaluator=judge,
+            harmful_prompts=dataset, inference_config=inference_config,
+            batch_size=batch_size,
+            starting_index=starting_index, end_index=end_index, seed=seed, metrics_name_to_failure_prompts=metrics_name_to_failure_prompts,
+        )
+    else:
+        exp = go_jailbreak(
+            model=model, method=cot_bypass, evaluator=judge,
+            harmful_prompts=dataset, inference_config=inference_config,
+            batch_size=batch_size,
+            starting_index=starting_index, end_index=end_index, seed=seed
+        )
+
+    free_gpu_memory(model)
+    record_dir = exp.log_dir
+    logger.info(f"Records are saved in {record_dir}")
+    all_results = go_evaluate_on_all_metrics(record_dir)
+
+    refusal_results = all_results["RefusalWordsEvaluator"]
+    success_count = count_success_on_failures(
+        refusal_results,
+        metrics_name_to_failure_prompts["RefusalWordsEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. RefusalWordsEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+    llama_guard_results = all_results["LlamaGuardEvaluator"]
+    success_count = count_success_on_failures(
+        llama_guard_results,
+        metrics_name_to_failure_prompts["LlamaGuardEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. LlamaGuardEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+    harm_bench_results = all_results["HarmBenchEvaluator"]
+    success_count = count_success_on_failures(
+        harm_bench_results,
+        metrics_name_to_failure_prompts["HarmBenchEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. HarmBenchEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+    strong_reject_results = all_results["StrongREJECTEvaluator"]
+    success_count = count_success_on_failures(
+        strong_reject_results,
+        metrics_name_to_failure_prompts["StrongREJECTEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. StrongREJECTEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+
+def count_success_on_failures(
+    eval_results,
+    failure_prompts,
+):
+    success_count = 0
+    for result in eval_results.record_list:
+        if result.datasample.original_harmful_prompt in failure_prompts:
+            if result.evaluate_result.is_harmful:
+                success_count += 1
+    return success_count
+
 
 def transform_original_prompts(
     dataset_name: str,
@@ -93,8 +185,99 @@ def transform_original_prompts(
     dataset.apply_transformation(
         fake_over_refusal,
         stored_path=os.path.join(os.path.dirname(__file__), "../transformed_datasets"),
-        batch_size=8,
+        batch_size=16,
     )
+
+def main_re_evaluate(
+    dataset_name: Literal[
+        "StrongReject", "HarmfulBehaviors", "HarmBench", "CatQA", "JBBHarmfulBehaviors"] = "StrongReject",
+    model_name: str = "openai/gpt-oss-20b",
+    starting_index: int = 0,
+    end_index: int = -1,
+    batch_size: int = 4,
+    seed: int = 42,
+    failure_cases_dir: pathlib.Path | str = None,
+    exp_records_path: str = None,
+
+):
+    logger.info(f"Loading transformed dataset {dataset_name} for FakeOverrefusal method.")
+    base_dir = os.path.join(os.path.dirname(__file__), "../transformed_datasets")
+    if dataset_name == "StrongReject":
+        dataset = StrongReject(data_path=os.path.join(base_dir, "strongreject_dataset_FakeOverrefusal.csv"))
+    elif dataset_name == "HarmfulBehaviors":
+        dataset = HarmfulBehaviors(data_path=os.path.join(base_dir, "harmful_behaviors_FakeOverrefusal.csv"))
+    elif dataset_name == "HarmBench":
+        dataset = HarmBench(data_path=os.path.join(base_dir, "harmbench_behaviors_text_all_FakeOverrefusal.csv"))
+    elif dataset_name == "CatQA":
+        dataset = CatQA(data_path=os.path.join(base_dir, "catqa_english_FakeOverrefusal.json"))
+    elif dataset_name == "JBBHarmfulBehaviors":
+        dataset = JBBHarmfulBehaviors(data_path=os.path.join(base_dir, "jbb-harmful-behaviors_FakeOverrefusal.csv"))
+    else:
+        raise ValueError(f"Unknown dataset name: {dataset_name}")
+
+    failure_cases_dir = os.path.join(failure_cases_dir, "re_evaluation_records_before_evaluation")
+    assert os.path.exists(failure_cases_dir), f"Failure cases directory {failure_cases_dir} does not exist."
+
+    failure_cases_json = [os.path.join(failure_cases_dir, f) for f in os.listdir(failure_cases_dir) if
+                          f.startswith("failure_cases_by_") and f.endswith(".json")]
+
+    def get_metric_name(file_path: str) -> str:
+        return file_path.split("failure_cases_by_")[-1].split(".json")[0]
+
+    def get_question_list(file_path: str) -> list:
+        import json
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        questions = data['failure_cases']
+        return questions
+
+    metrics_name_to_failure_prompts = {
+        get_metric_name(file_path): get_question_list(file_path) for file_path in failure_cases_json
+    }
+    # from breakoss.go_jailbreak import ExperimentFullRecords
+    # with open(os.path.join(exp_records_path, "records_before_evaluation.json"), "r") as f:
+    #     exp = ExperimentFullRecords.model_validate_json(f.read())
+    #
+    # record_dir = exp.log_dir
+    #
+    all_results = go_evaluate_on_all_metrics(exp_records_path)
+
+    refusal_results = all_results["RefusalWordsEvaluator"]
+    success_count = count_success_on_failures(
+        refusal_results,
+        metrics_name_to_failure_prompts["RefusalWordsEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. RefusalWordsEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+    llama_guard_results = all_results["LlamaGuardEvaluator"]
+    success_count = count_success_on_failures(
+        llama_guard_results,
+        metrics_name_to_failure_prompts["LlamaGuardEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. LlamaGuardEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+    harm_bench_results = all_results["HarmBenchEvaluator"]
+    success_count = count_success_on_failures(
+        harm_bench_results,
+        metrics_name_to_failure_prompts["HarmBenchEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. HarmBenchEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+    strong_reject_results = all_results["StrongREJECTEvaluator"]
+    success_count = count_success_on_failures(
+        strong_reject_results,
+        metrics_name_to_failure_prompts["StrongREJECTEvaluator"]
+    )
+    logger.info(f"Success count w.r.t. StrongREJECTEvaluator is {success_count}")
+    logger.info(f"Total ASR increased by +{success_count / len(dataset) * 100:.2f}%.")
+
+
+def rephrase_until_success():
+    ...
+
 
 if __name__ == "__main__":
     fire.Fire()
