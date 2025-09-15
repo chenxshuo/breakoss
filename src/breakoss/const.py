fc7a453206bc446c99b41367e27df5d7c04502a7
chenshuo <c1094829085@qq.com>
2025-09-15 10:42:38 +0200

    update supported models

--- src/breakoss/const.py
+++ src/breakoss/const.py
@@ -1,8 +1,10 @@
 SUPPORTED_MODELS = [
     "openai/gpt-oss-20b",
     "openai/gpt-oss-120b",
-    # "microsoft/phi-4-reasoning",
-    # "qwen/qwen3-4b-thinking-2507",
+    "microsoft/Phi-4-reasoning-plus",
+    "Qwen/Qwen3-4B-Thinking-2507",
+    "Qwen/Qwen3-30B-A3B-Thinking-2507",
+
     # "RealSafe/RealSafe-R1-7B"
 ]
 
