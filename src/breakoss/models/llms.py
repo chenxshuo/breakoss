9b0111485fddeb49c43145b49f547dc3f8993c3b
chenshuo <c1094829085@qq.com>
2025-09-15 10:42:30 +0200

    add apply_chat_template

--- src/breakoss/models/llms.py
+++ src/breakoss/models/llms.py
@@ -83,7 +83,7 @@ class LLMHF:
             outputs[0][inputs["input_ids"].shape[1] :], skip_special_tokens=False
         )
 
-    def chat_batch(self, *, prompts: list[str], inference_config: InferenceConfig, system_prompt: str = None) -> list[str]:
+    def chat_batch(self, *, prompts: list[str], inference_config: InferenceConfig, system_prompt: str = None, apply_chat_template: bool=True) -> list[str]:
         """Generate chat responses for a batch of prompts using the loaded model.
 
         Args:
@@ -94,13 +94,18 @@ class LLMHF:
             List of generated text responses from the model.
         """
         messages = [[{"role": "user", "content": prompt}] for prompt in prompts]
-        inputs = self.tokenizer.apply_chat_template(
-            messages,
-            add_generation_prompt=True,
-            return_tensors="pt",
-            return_dict=True,
-            padding=True,
-        ).to(self.model.device)
+        if apply_chat_template:
+            inputs = self.tokenizer.apply_chat_template(
+                messages,
+                add_generation_prompt=True,
+                return_tensors="pt",
+                return_dict=True,
+                padding=True, truncation=True
+            ).to(self.model.device)
+        else:
+            inputs = self.tokenizer(
+                prompts, return_tensors="pt", padding=True, truncation=True
+            ).to(self.model.device)
 
         outputs = self.model.generate(
             **inputs,
